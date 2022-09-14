76bde5a6656d323ab91795417a1a80e6ddcadac9
jrbrawner <jrbbrawner@gmail.com>
2022-09-14 14:45:36 -0500

    working on async db setup

---
+++ fast-api/api/dependencies.py
@@ -0,0 +1,6 @@
+from api.db.session import async_session
+
+async def get_db():
+    async with async_session() as session:
+        async with session.begin():
+            yield session
--- fast-api/api/main.py
+++ fast-api/api/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from api.db.session import Base, engine
 
 from api.routers import hello_world
 
 app = FastAPI()
 
 app.include_router(hello_world.router)
+
+@app.on_event("startup")
+async def startup():
+    # create db tables
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
