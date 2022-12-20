0325a3dd7b8603dd45d40ae6fbf0f428ec2bbd05
jrbrawner <jrbbrawner@gmail.com>
2022-12-20 16:13:58 -0600

    test

--- api/src/mitre/router.py
+++ api/src/mitre/router.py
@@ -31,7 +31,4 @@ def get_mitre_tactic_techniques(type: MitreLookup, term: str, db: Session = Depe
         tactic = services.get_mitre_tactic_name(db, term)
         if tactic is None:
             raise HTTPException(404, 'No tactic found with that name.')
-        
-        
-        
         return tactic
