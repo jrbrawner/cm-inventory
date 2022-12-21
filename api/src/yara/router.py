e6dbb429dc2a533e2b00bf3fc93b724426b70664
jrbrawner <jrbbrawner@gmail.com>
2022-12-20 20:07:04 -0600

    working on yara routes

--- api/src/yara/router.py
+++ api/src/yara/router.py
@@ -7,9 +7,13 @@ from typing import List
 
 router = APIRouter()
 
-@router.post("/yara", response_model=List[YaraSchema], tags=['yara'])
-def create_yara_rules(rule_text: str, db: Session = Depends(get_db)):
-    yara_rule_list = services.create_yara_rules(db, rule_text)
+@router.post("/yara", response_model=List[YaraCreate], tags=['yara'])
+def create_yara_rules(rules_text: str, db: Session = Depends(get_db)):
+    yara_rule_list = services.create_yara_rules(db, rules_text)
     if yara_rule_list is None:
         raise HTTPException(400, 'Error in rule creation.')
     return yara_rule_list
+
+@router.get("/yara", response_model=YaraSchema, tags=['yara'])
+def get_yara_rule(rule_name: str, db: Session = Depends(get_db)):
+    pass
--- api/src/yara/services.py
+++ api/src/yara/services.py
@@ -7,11 +7,13 @@ from YaraParser import MultiParser
 def create_yara_rules(db: Session, rule_text: str):
     parser = MultiParser(rule_text)
     rules = parser.get_rules_dict()
+    keyword_list = ['tactic', 'technique', 'subtechnique']
     yara_rule_list = []
 
     for name, rule in rules.items():
+        keyword_values = parser.get_meta_fields(rule['rule_meta_kvp'], meta_keyword_list=keyword_list)
         yara_rule = YaraRule(name = rule['rule_name'], meta = rule['rule_meta'], strings = rule['rule_strings'], conditions = rule['rule_conditions'], 
-        raw_text = rule['raw_text'])
+        raw_text = rule['raw_text'], tactic = keyword_values[0], technique = keyword_values[1], subtechnique = keyword_values[2])
         db.add(yara_rule)
         db.commit()
         yara_rule_list.append(yara_rule)
