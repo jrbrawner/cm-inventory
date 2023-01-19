ffa2265efd73ede35988f17c1abe21aa28e1a789
jrbrawner <jrbbrawner@gmail.com>
2023-01-18 20:50:10 -0500

    generation of yara rules working

--- api/src/mitre/router.py
+++ api/src/mitre/router.py
@@ -15,6 +15,7 @@ from mitreattack.navlayers import Layer, ToSvg, SVGConfig
 import re
 import json
 from src.yara.schemas import YaraSchema
+from collections import Counter
 
 router = APIRouter()
 
@@ -57,21 +58,27 @@ def get_mitre_tactic_techniques(
 @router.get("/mitre/generate-heatmap", tags=['mitre'])
 def generate_heatmap(db: Session = Depends(get_db)):
 
-    rules_to_viz = db.query(YaraRule).filter(YaraRule.techniques.isnot(None)).all()
-    pattern = "[T][0-9][0-9][0-9][0-9]"
-    techniques_list = []
-    technique_list = []
+    yara_rules_to_viz = db.query(YaraRule).filter(YaraRule.techniques != None).all()
+    yara_techniques_list = []
+    yara_technique_list = []
+    yara_technique_set = set()
+    
+    for rule in yara_rules_to_viz:
+        for technique in rule.techniques:
+            yara_technique_list.append(technique.id)
+    
+    for technique in yara_technique_list:
+        occ = Counter(yara_technique_list)
+        technique_count = {technique: occ[technique]}
+        yara_technique_set.add(tuple(technique_count.items()))
 
-    for i in rules_to_viz:
-        model = YaraSchema.from_orm(i)
-        techniques = [x for x in model.techniques]
-        for i in techniques:
-            techniques_list.append(i)
-
-    for i in technique_list:
-        techniques_list.append({
-                "techniqueID": i,
-                "score": 1,
+    
+    for i in yara_technique_set:
+        technique = dict(i)
+        print(technique)
+        yara_techniques_list.append({
+                "techniqueID": str(*technique),
+                "score": technique.pop(*technique),
                 "metadata": [
                     {
                         "name": "Coverage",
@@ -83,27 +90,28 @@ def generate_heatmap(db: Session = Depends(get_db)):
         layer = {
         "name": "test example",
         "versions": {
-            "layer": "4.3",
-            "navigator": "4.3"
+            "layer": "4.4",
+            "navigator": "4.8.0"
         },
         "sorting": 3, # descending order of score
-        "description": "An example layer where all techniques have a randomized score",
+        "description": "testing generation of heatmaps based on countermeasure coverage",
         "domain": "enterprise-attack",
-        "techniques": techniques_list,
+        "techniques": yara_techniques_list,
         }
     
-        with open('test_layer.json', "w", encoding="utf-8") as file:
+        with open('src/mitre/layers/test_layer.json', "w", encoding="utf-8") as file:
             json.dump(layer, file, ensure_ascii=False, indent=4)
         file.close()
-
+        
     return 'Layer created with Yara rules data.'
 
+@router.get("/mitre/create-layer", tags=['mitre'])
 def load_layer_to_svg():
 
     lay = Layer()
-    lay.from_file(path + "/test_layer.json")
-    # Using taxii server for template
+    lay.from_file("src/mitre/layers/test_layer.json")
 
+    # Using taxii server for template
     config = SVGConfig(width=12, height=12, headerHeight=1, unit="in",\
         showSubtechniques="all", font="sans-serif", tableBorderColor="#6B7279",\
         showHeader=True, legendDocked=True, legendX=0, legendY=0, legendWidth=2,\
@@ -111,8 +119,7 @@ def load_layer_to_svg():
         showAbout=True, showDomain=True, border=0.104)
 
     viz = ToSvg(domain='enterprise', source="local",\
-         resource=path+"/mitre-enterprise-attack.json", config=config)
+         resource="src/mitre/data/mitre-enterprise-attack.json", config=config)
     
-    
-    viz.to_svg(layerInit=lay, filepath="test.svg")
-    print('Test SVG created.')
+    viz.to_svg(layerInit=lay, filepath="src/mitre/viz/test.svg")
+    return 'Test SVG created.'
