c1fbd8f291edd9bd7cd68f01f5ec9dc911530258
jrbrawner <jrbbrawner@gmail.com>
2023-02-20 20:53:28 -0500

    working on snort engine integration

--- api/src/snort_engine/router.py
+++ api/src/snort_engine/router.py
@@ -12,9 +12,38 @@ def get_available_configurations():
         raise HTTPException(400, 'Error in retrieving available configurations.')
     return conf
 
+@router.get("/api/snort-engine/configuration/{name}", response_class=PlainTextResponse, tags=['snort-engine'])
+def get_available_configurations(name: str):
+    conf = services.get_configuration_name(name)
+    if conf is None:
+        raise HTTPException(400, 'Error in retrieving available configurations.')
+    return conf
+
 @router.post("/api/snort-engine/configuration", response_class=PlainTextResponse, tags=['snort-engine'])
 def create_new_configuration(config_name: str, HOME_NET: str, EXTERNAL_NET: str = '!$HOME_NET'):
     result = services.create_configuration(config_name, HOME_NET, EXTERNAL_NET)
     if result is None:
         raise HTTPException(400, 'Error in creating new configuration.')
+    return result
+
+@router.post("/api/snort-engine/read-pcap", response_class=PlainTextResponse, tags=['snort-engine'])
+def read_pcap(pcap_file: UploadFile):
+    result = services.read_pcap(pcap_file)
+    if result is None:
+        raise HTTPException(400, 'Error in sending pcap file or returning result.')
+    return result
+
+@router.post("/api/read-pcap-detailed", response_class=PlainTextResponse, tags=['snort-engine'])
+def read_pcap_detailed(pcap_file: UploadFile, show_raw_packet_data: bool = False):
+    result = services.read_pcap_detailed(pcap_file, show_raw_packet_data)
+    if result is None:
+        raise HTTPException(400, 'Error in sending pcap file or returning result.')
+    return result
+
+
+@router.post("/api/analyze-pcap", response_class=PlainTextResponse, tags=['snort-engine'])
+def analyze_pcap(pcap_file: UploadFile):
+    result = services.analyze_pcap(pcap_file)
+    if result is None:
+        raise HTTPException(400, 'Error in analyzing pcap')
     return result
--- api/src/snort_engine/services.py
+++ api/src/snort_engine/services.py
@@ -1,13 +1,56 @@
 import requests
+from fastapi import UploadFile
+import tempfile
 
 snort_url = 'https://jrbrawner-vigilant-space-broccoli-qg9qw4vg5gghx4rr-80.preview.app.github.dev'
 
-def get_available_configurations():
-    conf = requests.get(f'{snort_url}/configuration').content
-    
-    return conf
+def get_available_configurations() -> str:
+    """Display available configurations from Snort Engine."""
+    conf = requests.get(f'{snort_url}/configuration')
+    return conf.content
 
-def create_configuration(config_name: str, HOME_NET: str, EXTERNAL_NET: str):
-    request_data = {"config_name": config_name, "HOME_NET": HOME_NET, "EXTERNAL_NET": EXTERNAL_NET}
+def get_configuration_name(name: str) -> str:
+    """Display configuration text from specified file."""
+    conf = requests.get(f'{snort_url}/configuration/{name}')
+    return conf.content
+
+def create_configuration(config_name: str, HOME_NET: str, EXTERNAL_NET: str) -> str:
+    """Create a new configuration based on provided parameters."""
+    params = {"config_name": config_name, "HOME_NET": HOME_NET, "EXTERNAL_NET": EXTERNAL_NET}
     configuration = requests.post(url=f"{snort_url}/configuration",
-                                  data=request_data)
+                                  params=params)
+    
+    return configuration.content
+
+def read_pcap(pcap_file: UploadFile) -> str:
+    """Send a pcap file to snort engine to inspect and display the return the result."""
+    contents = pcap_file.file.read()
+    files = {'pcap_file': ('pcap_file', contents)}
+    result = requests.post(f"{snort_url}/read-pcap",
+                           files=files)
+    return result.content
+
+def read_pcap_detailed(pcap_file: UploadFile, show_raw_packet_data: bool) -> str:
+    contents = pcap_file.file.read()
+    files = {'pcap_file': ('pcap_file', contents)}
+    params = {"show_raw_packet_data": show_raw_packet_data}
+    result = requests.post(f"{snort_url}/read-pcap-detailed",
+                           files=files,
+                           params=params)
+    
+    return result.content
+
+def analyze_pcap(pcap_file: UploadFile) -> str:
+    temp_file_rules = tempfile.NamedTemporaryFile(delete=False)
+    rules = 'alert tcp any any -> any any (msg:"This is a test";) '
+    f = open(temp_file_rules.name, 'w')
+    f.write(rules)
+
+    contents = pcap_file.file.read()
+    files = {'pcap_file': ('pcap_file', contents), 'rules_file': ('rules_file', temp_file_rules)}
+    f.close()
+
+    result = requests.post(f"{snort_url}/analyze-pcap",
+                           files=files)
+    
+    return result.content
